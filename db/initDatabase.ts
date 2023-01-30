f4367960731309c9bc53cf529d7ca93eeef2817d
Jose Fabio <senorbinario@tuta.io>
2023-01-29 23:35:45 -0600

    Missing file in last commit

--- db/initDatabase.ts
+++ db/initDatabase.ts
@@ -1,15 +1,13 @@
 import loki from "lokijs";
-import { createRequire } from "node";
-
-const require = createRequire(import.meta.url);
-const lfsa = require("../utils/loki-fs-sync-adapter.js");
+import LokiFsSyncAdapter from "../utils/loki-fs-sync-adapter.ts";
 
 // Ejemplos en https://github.com/techfort/LokiJS/wiki/Query-Examples.
 
 // Las opciones de la base de datos.
-const adapter = new lfsa();
-const dbOptions = {
-  adapter,
+const dbOptions: Partial<LokiConstructorOptions> &
+  Partial<LokiConfigOptions> &
+  Partial<ThrottledSaveDrainOptions> = {
+  adapter: LokiFsSyncAdapter,
   // autosave: true,
   // El autoupdate funciona extraño. Es mejor usar manualmente db.update();
   // autoupdate: true, // https://github.com/techfort/LokiJS/wiki/Autoupdating-Collections
