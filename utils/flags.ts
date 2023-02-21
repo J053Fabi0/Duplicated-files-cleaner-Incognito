1335773a0137cf8f6d0c7c3362233bfe09e7fd6f
Jose Fabio <senorbinario@tuta.io>
2023-02-21 12:19:19 -0600

    console.logs for flags

--- utils/flags.ts
+++ utils/flags.ts
@@ -1,3 +1,5 @@
+console.log(Deno.args);
+
 const flags = {
   // --keep-status: Only start the nodes that were online before the script started.
   keepStatus: Deno.args.includes("--keep-status"),
@@ -6,5 +8,7 @@ const flags = {
   // --only-offline: Skip the nodes that are online. This skips the check for the nodes that are in or about to be in committee.
   onlyOffline: Deno.args.includes("--only-offline"),
 };
+
+console.log(flags);
 
 export default flags;
