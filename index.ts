fcaaa4df93ac45c451f9a2cab49d4bc49aba31cf
Jose Fabio <senorbinario@tuta.io>
2023-03-17 19:27:23 -0600

    Fix help message

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import run from "./run/run.ts";
 
 // if --help or -h is passed, show the help message
 if (Deno.args.includes("--help") || Deno.args.includes("-h")) {
-  console.log(`Usage: deno run index.ts`);
-  console.log(`Usage to copy files: deno run index.ts [fromNodeIndex] [toNodeIndex] [shard=beacon]`);
+  console.log(`Usage: deno task run index.ts`);
+  console.log(`Usage to copy files: deno task run index.ts [fromNodeIndex] [toNodeIndex] [shard=beacon]`);
   Deno.exit();
 }
 
