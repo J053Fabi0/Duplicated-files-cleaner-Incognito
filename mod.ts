904741cabdffd5d95158d9835c8014d547423763
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-03-21 20:04:22 -0600

    Missing exports for Info type and copyFileOrDir

--- mod.ts
+++ mod.ts
@@ -4,6 +4,7 @@ export default DuplicatedFilesCleaner;
 export { default as spawnPromise } from "./utils/spawnPromise.ts";
 export { default as binaryWrapper } from "./utils/binaryWrapper.ts";
 
+export { copyFileOrDir } from "./src/copyData.ts";
 export { shardsNames } from "./types/shards.type.ts";
 export { default as getFiles } from "./utils/getFiles.ts";
 export { dockerPs, docker, df } from "./utils/commands.ts";
@@ -11,6 +12,7 @@ export { default as normalizeShards } from "./utils/normalizeShards.ts";
 export { default as repeatUntilNoError } from "./utils/repeatUntilNoError.ts";
 
 // Types
+export type { Info } from "./src/getInfo.ts";
 export type { LDBFile } from "./utils/getFiles.ts";
 export type { default as ShardsNumbers, ShardsNames, ShardsStr } from "./types/shards.type.ts";
 export type { default as Constants, Instruction, ValidatorPublicKeys } from "./types/constants.type.ts";
