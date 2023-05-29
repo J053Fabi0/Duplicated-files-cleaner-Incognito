3af3b4c6fe599237921060f670e060a57d36d575
Jose Fabio <J053Fabi0@users.noreply.github.com>
2023-05-29 16:19:04 -0600

    Updated dependencies

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts";
-export { join } from "https://deno.land/std@0.185.0/path/mod.ts";
+export { join } from "https://deno.land/std@0.189.0/path/mod.ts";
 export { MultiProgressBar } from "https://deno.land/x/progress@v1.3.8/mod.ts";
-export { readableStreamFromReader } from "https://deno.land/std@0.185.0/streams/mod.ts";
+export { readableStreamFromReader } from "https://deno.land/std@0.189.0/streams/mod.ts";
